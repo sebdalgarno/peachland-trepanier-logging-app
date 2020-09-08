c48e28151a0596bb73e540420b3835bab6135117
Seb Dalgarno <seb@poissonconsulting.ca>
2020-09-08 12:41:53 -0700

    replace css with tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -60,148 +60,49 @@
 </script>
 
 <style>
-  .sliderContainer {
-    margin: 0 auto;
-    padding-right: 2rem;
-    padding-left: 2.3rem;
-    width: 90%;
-  }
 
-  p {
-    color: white;
-    margin: 0.2em;
-    /* padding: 0; */
-  }
-
-  .year {
-    text-align: center;
-    padding-bottom: 0;
-    margin-bottom: 0;
-    /* padding-left: 0.5em; */
-    font-size: 3em;
-    color: #bdbdbd;
-  }
-
-  .pane {
-    display: flex;
-    flex-direction: column;
-    max-height: 100vh;
-    /* background-color: #000; */
-  }
-
-  .info {
-    background: rgba(0, 0, 0, 0.7);
-    border-top-right-radius: 5%;
-    position: fixed;
-    /* min-width: 500px;
-    min-height: 300px; */
-    width: 40%;
-    height: 40%;
-    bottom: 0;
-    left: 0;
-    z-index: 10;
-    /* display: grid; */
-    grid-gap: 1rem;
-    padding: 1em;
-    /* overflow: scroll; */
-  }
-
-  .map {
-    width: 100%;
-    height: 100%;
-  }
-
-  .title {
-    color: #bdbdbd;
-    padding: 0 1rem 0 2rem;
-    font-size: 1.5rem;
-  }
-
-  .inline {
-    display: inline-block;
-  }
-
-  .buttons {
-    /* bottom: 40%; */
-    /* position: relative; */
-    width: 0 auto;
-    /* border-top-right-radius: 5%; */
-  }
-
-  .extras {
-    position: absolute;
-    left: 0rem;
-    background-color: rgba(0, 0, 0, 0);
-    border-radius: 0;
-    padding: 5px;
-    z-index: 1;
-    /* text-align: left; */
-  }
-
-  .legend {
-    bottom: 40%;
-    padding-left: 1rem;
-    background-color: rgba(0, 0, 0, 0.7);
-    position: absolute;
-    left: 0rem;
-    padding: 5px;
-    z-index: 1;
-    /* border-bottom-right-radius: 5%; */
-    border-top-right-radius: 5%;
-    /* position: relative; */
-    color: #bdbdbd;
-  }
-
-  @media (max-width: 768px) {
-    .info {
-      border-top-right-radius: 0;
-      width: 100%;
-      height: 40%;
-    }
-
-    .title {
-      display: none;
-    }
-
-    .story {
-      display: none;
-    }
-  }
 </style>
 
 <header
-  class="py-2 w-full absolute bg-black text-gray-500 text-1.5 p-5 bg-opacity-50
-  z-50">
+  class="invisible absolute py-1 w-full bg-black text-gray-400 text-1.5 p-5
+  bg-opacity-50 z-50 sm:visible">
   A History of Logging in Peachland and Trepanier Watersheds 1965-2020
   <Modal>
     <ModalAbout />
   </Modal>
 </header>
 
-<div class="legend ">
-  <Legend {palette} {single} {map_palette} {map_palette_single} />
-</div>
+<div class="flex absolute bottom-0 w-full md:w-5/12">
+  <div class="w-10/12 z-50 bg-black bg-opacity-50 p-2">
+    <div class="absolute">
+      {#if !single}
+        <Button caption={'by year'} on:single-year={toggleSingle} />
+      {:else}
+        <Button caption={'all years'} on:single-year={toggleSingle} />
+      {/if}
 
-<div class="info ">
-  <div class="buttons extras ">
-    {#if !single}
-      <Button caption={'by year'} on:single-year={toggleSingle} />
-    {:else}
-      <Button caption={'all years'} on:single-year={toggleSingle} />
-    {/if}
-    <Button caption={'+'} on:add-year={addYear} />
-    <Button caption={'-'} on:minus-year={minusYear} />
+    </div>
+    <div class="text-center">
+      <Button outline={false} caption={'-'} on:minus-year={minusYear} />
 
+      <div class="inline-block">
+        <p class="text-5xl text-gray-400">{year}</p>
+      </div>
+      <Button outline={false} caption={'+'} on:add-year={addYear} />
+    </div>
+
+    <!-- <div class="buttons extras increment " /> -->
+
+    <div class="mx-auto px-4 w-11/12">
+      <Slider class="mdc-slider" bind:value={slider_value} />
+    </div>
+    <div class="h-40">
+      <!-- <Chart {year} {single} {palette} {map_palette} {map_palette_single} /> -->
+    </div>
   </div>
-  <p class="text-2">{year}</p>
-  <div class="buttons extras increment " />
-
-  <div class="sliderContainer">
-    <Slider class="mdc-slider" bind:value={slider_value} />
+  <div class="w-2/12 p-2 bottom-0 bg-black bg-opacity-50 z-10 text-gray-400 ">
+    <Legend {palette} {single} {map_palette} {map_palette_single} />
   </div>
-  <Chart {year} {single} {palette} {map_palette} {map_palette_single} />
 </div>
 
-<div class="w-full h-full">
-  <Map {year} {single} {map_palette} {map_palette_single} {bounds} />
-</div>
+<Map {year} {single} {map_palette} {map_palette_single} {bounds} />
